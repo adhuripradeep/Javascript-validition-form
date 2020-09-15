ae64d4e6a903743a09b2d1a09a0f825c39038d7a
aduri pradeep <you@example.com>
2020-09-15 19:14:03 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function myFunction() {
+  var a = document.getElementById("Fname").value;
+  var b = document.getElementById("Lname").value;
+  var c = document.getElementById("email").value;
+  var d = document.getElementById("password").value;
+  var e = document.getElementById("cpassword").value;
+  if (a == "" || b == "" || c == "" || d == "" || e == "") {
+    alert("Fil the text box");
+  } else {
+    console.log(x);
+  }
+}
